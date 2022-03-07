c869187692f9d6692505944efbf3577148c92afd
dao <gary.vo32@gmail.com>
2022-03-07 14:21:35 -0800

    added flexibility in grid size when user resets canvas

--- js/main.js
+++ js/main.js
@@ -1,28 +1,45 @@
-let num = 16;
-let canvasStyle = `width:${(1/num)*100}%; height:${(1/num)*100}%; border: 2px solid red; background: white;`;
+let num = 30;
+let canvasStyle = `width:${(1/num)*100}%; height:${(1/num)*100}%; border: 1px solid lightgray; background: white;`;
 const canvas = document.querySelector('.canvas')
 
-
-for (let i = 0; i < num*num; i++) {
-  const block = document.createElement('div') //.classList.add('.block');
-  // color in when the curser enters the box
-  block.addEventListener('mouseenter', draw);
-  //block.setAttribute('class', 'block');
-  block.setAttribute('style', canvasStyle);
-  canvas.appendChild(block);
+function setCanvas(num) {
+  for (let i = 0; i < num * num; i++) {
+    const block = document.createElement('div') //.classList.add('.block');
+    // color in when the curser enters the box
+    block.addEventListener('mouseenter', draw);
+    //block.setAttribute('class', 'block');
+    block.setAttribute('style', canvasStyle);
+    canvas.appendChild(block);
+  }
 }
 
+setCanvas(num);
 
 function draw(e) {
-  console.log(e.target.style.background = 'red');
+  console.log(e.target.style.backgroundColor = 'salmon');
 }
 
 //reset button
 function reset() {
+  let properNumber = false;
   let blocks = document.querySelectorAll('div');
+
   blocks.forEach((square) => {
-    console.log(square.setAttribute('style', canvasStyle));
+    square.remove();
+    //console.log(square.setAttribute('style', canvasStyle));
   });
+
+  //test if user input is a proper number no more than 100.
+  while (!properNumber) {
+    num = +prompt('change the grid size. 100 is the max.', '');
+    if (num <= 100 && num > 0) {
+      properNumber = true;
+    }
+  }
+  console.log(num);
+  canvasStyle =`width:${(1/num)*100}%; height:${(1/num)*100}%; border: 1px solid lightgray; background: white;`;
+  setCanvas(num);
 }
+
 const resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', reset)

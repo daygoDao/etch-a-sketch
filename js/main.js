77f9c367203057216947ac4b6ad757a2c4129094
dao <gary.vo32@gmail.com>
2022-03-07 13:37:12 -0800

    added a reset button!

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 let num = 16;
+let canvasStyle = `width:${(1/num)*100}%; height:${(1/num)*100}%; border: 2px solid red; background: white;`;
 const canvas = document.querySelector('.canvas')
+
 
 for (let i = 0; i < num*num; i++) {
   const block = document.createElement('div') //.classList.add('.block');
   // color in when the curser enters the box
   block.addEventListener('mouseenter', draw);
-
-  block.setAttribute('style', `width:${(1/num)*100}%; height:${(1/num)*100}%; border: 2px solid red;`);
+  //block.setAttribute('class', 'block');
+  block.setAttribute('style', canvasStyle);
   canvas.appendChild(block);
 }
 
 
 function draw(e) {
-  console.log(e.target.classList.add('colorIn'));
+  console.log(e.target.style.background = 'red');
 }
+
+//reset button
+function reset() {
+  let blocks = document.querySelectorAll('div');
+  blocks.forEach((square) => {
+    console.log(square.setAttribute('style', canvasStyle));
+  });
+}
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', reset)

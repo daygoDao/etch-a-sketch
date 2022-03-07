7958988ad9fe4ee7507d34ba9a2d5097924f36db
dao <gary.vo32@gmail.com>
2022-03-07 11:42:58 -0800

    able to color on the canvas

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ const canvas = document.querySelector('.canvas')
 
 for (let i = 0; i < num*num; i++) {
   const block = document.createElement('div') //.classList.add('.block');
+  // color in when the curser enters the box
+  block.addEventListener('mouseenter', draw);
+
   block.setAttribute('style', `width:${(1/num)*100}%; height:${(1/num)*100}%; border: 2px solid red;`);
   canvas.appendChild(block);
+}
+
+
+function draw(e) {
+  console.log(e.target.classList.add('colorIn'));
 }
